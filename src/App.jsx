c850c6afadbebd290815bd838fc4c0747f141080
lipee1307 <felipesantana.rds@gmail.com>
2026-02-04 11:47:02 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+    <section>
+     <textarea placeholder="Descreva seu problema"
+     ></textarea>
+     <button>Enviar</button>
+    </section>
+      
+    </>
+  )
+}
+
+
